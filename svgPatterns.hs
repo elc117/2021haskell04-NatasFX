d3dddbe5fc3ee912908e692b6bed555dfadf992c
NatasFX <nataschmitt02@hotmail.com>
2021-06-06 10:45:18 -0300

    Done part 2

--- svgPatterns.hs
+++ svgPatterns.hs
@@ -9,29 +9,35 @@ type Circle    = (Point,Float)
 -- Paletas
 -------------------------------------------------------------------------------
 
--- Paleta (R, G, B) só com tons de verde "hard-coded" 
--- (pode ser melhorado substituindo os valores literais por parâmetros)
--- Além disso, o que acontecerá se n for muito grande ou negativo?
-greenPalette :: Int -> [(Int,Int,Int)]
-greenPalette n = [(0, 80+i*10, 0) | i <- [0..n] ]
-
 -- Paleta com n valores retirados de uma lista com sequências de R, G e B 
 -- O '$' é uma facilidade sintática que substitui parênteses
 -- O cycle é uma função bacana -- procure saber mais sobre ela :-)
-rgbPalette :: Int -> [(Int,Int,Int)]
-rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
+paletteRed :: Int -> [Float]
+paletteRed n   = take n (cycle (replicate 90 255 ++ reverse [0,2.9..255] ++ replicate 90 0 ++ [0,2.9..255] ))
 
+paletteGreen :: Int -> [Float]
+paletteGreen n = take n (cycle ([0,2.9..255] ++ replicate 90 255 ++ reverse [0,2.9..255] ++ replicate 90 0))
 
+paletteBlue :: Int -> [Float]
+paletteBlue n  = take n (cycle (replicate 90 0 ++ [0,2.9..255] ++ replicate 90 255 ++ reverse [0,2.9..255]))
+
+rgbPalette :: Int -> [(Float,Float,Float)]
+rgbPalette n = zip3 (paletteBlue n) (paletteGreen n) (paletteRed n)
 
 -------------------------------------------------------------------------------
 -- Geração de retângulos em suas posições
 -------------------------------------------------------------------------------
 
-genRectsInLine :: Int -> [Rect]
-genRectsInLine n  = [((m*(w+gap), 0.0), w, h) | m <- [0..fromIntegral (n-1)]]
+genRectsInLineRev :: Int -> Float -> Float -> [Rect]
+genRectsInLineRev n width gap = [((width - m*(w+gap)-gap-w, m*(h+gap)+gap), w, h) | m <- [0..fromIntegral (n-1)]]
   where (w,h) = (50,50)
-        gap = 10
 
+genRectsInLine :: Int -> Float -> [Rect]
+genRectsInLine n gap = [((m*(w+gap)+gap, m*(h+gap)+gap), w, h) | m <- [0..fromIntegral (n-1)]]
+  where (w,h) = (50,50)
+
+genRectsDiagonals :: Int -> Float -> Float -> [Rect]
+genRectsDiagonals n width gap = genRectsInLineRev n width gap ++ genRectsInLine n gap
 
 -------------------------------------------------------------------------------
 -- Strings SVG
@@ -39,13 +45,12 @@ genRectsInLine n  = [((m*(w+gap), 0.0), w, h) | m <- [0..fromIntegral (n-1)]]
 
 -- Gera string representando retângulo SVG 
 -- dadas coordenadas e dimensões do retângulo e uma string com atributos de estilo
-svgRect :: Rect -> String -> String 
-svgRect ((x,y),w,h) style = 
-  printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
+svgRect :: Rect -> String -> String
+svgRect ((x,y),w,h) = printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h
 
 -- String inicial do SVG
 svgBegin :: Float -> Float -> String
-svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+svgBegin = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n"
 
 -- String final do SVG
 svgEnd :: String
@@ -53,8 +58,8 @@ svgEnd = "</svg>"
 
 -- Gera string com atributos de estilo para uma dada cor
 -- Atributo mix-blend-mode permite misturar cores
-svgStyle :: (Int,Int,Int) -> String
-svgStyle (r,g,b) = printf "fill:rgb(%d,%d,%d); mix-blend-mode: screen;" r g b
+svgStyle :: (Float,Float,Float) -> String
+svgStyle (r,g,b) = printf "fill:rgb(%.2f,%.2f,%.2f,.8); mix-blend-mode: screen;" r g b
 
 -- Gera strings SVG para uma dada lista de figuras e seus atributos de estilo
 -- Recebe uma função geradora de strings SVG, uma lista de círculos/retângulos e strings de estilo
@@ -66,14 +71,13 @@ svgElements func elements styles = concat $ zipWith func elements styles
 -------------------------------------------------------------------------------
 
 main :: IO ()
-main = do
-  writeFile "rects.svg" $ svgstrs
+main =
+  writeFile "rects.svg" svgstrs
   where svgstrs = svgBegin w h ++ svgfigs ++ svgEnd
         svgfigs = svgElements svgRect rects (map svgStyle palette)
-        rects = genRectsInLine nrects
-        palette = rgbPalette nrects
-        nrects = 10
-        (w,h) = (1500,500) -- width,height da imagem SVG
-
-
+        rects = genRectsDiagonals nrects w gap
+        palette = rgbPalette (nrects*2)
+        nrects = 720
+        (w,h) = (500,500) -- width,height da imagem SVG
+        gap = -49
 
